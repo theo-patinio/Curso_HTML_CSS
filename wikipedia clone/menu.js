24f853cf5d35eec26c518676f526f3252faf14db
Theo Patinio <theo@frika.com.ar>
2022-06-02 08:31:37 -0300

    menu desplegable

---
+++ wikipedia clone/menu.js
@@ -0,0 +1,10 @@
+document.getElementById("button").addEventListener("click", function() {
+    var box1=document.getElementById("desp__contenedor");
+    if (box1.style.display=="none") {
+        box1.style.display="block";
+    }
+    else{
+        box1.style.display="none";
+    }
+})
+
