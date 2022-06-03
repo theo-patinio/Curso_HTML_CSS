4dd2d6806bc405894ca0d25700a76159f77c3919
theo Patinio <theo@frika.com.ar>
2022-06-02 23:08:34 -0300

    mejorando animacion desplegable

--- wikipedia clone/menu.js
+++ wikipedia clone/menu.js
@@ -1,10 +1,22 @@
 document.getElementById("button").addEventListener("click", function() {
     var box1=document.getElementById("desp__contenedor");
+    var box2=document.getElementById("line");
+    var box3=document.getElementById("button");
+    var box4=document.getElementById("chevron");
+
     if (box1.style.display=="none") {
-        box1.style.display="block";
+        box1.style.display="flex";
+        box2.style.width="80%";
+        box3.style.border="2px solid #36c";
+        box4.className="fa-solid fa-chevron-up";
     }
     else{
         box1.style.display="none";
+        box2.style.width="45%";
+        box3.style.border="20px solid white";
+        box4.className="fa-solid fa-chevron-down";
     }
 })
+
+
 
